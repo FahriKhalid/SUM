96ce2a1e7717376a114c90b6372e86bdfb4dd21d
GujilGujil <fahri.halid@gmail.com>
2021-03-31 12:05:17 +0800

    update

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -264,7 +264,7 @@
 
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-    <script src="{{ asset('js/script_ver1.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 
     @yield('footer')
 
